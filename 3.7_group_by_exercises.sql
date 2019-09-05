6313ff528aef3a24127bd839f2a8fe9135db8200
Adam H <raymond.a.herman@gmail.com>
2019-09-04 20:07:04 -0500

    3.7_group_by_exercises.sql FINAL

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -17,54 +17,60 @@ from titles
 
 
 -- 3 Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
-
-    -- not sure why or how example shows statring with 'Eldridge' mine starts with 'Eastman'
     
 select last_name, count(last_name)
 from employees
-where last_name like 'e%'
+where last_name like 'e%e'
 group by last_name
-order by count() = 1
--- limit 20
 
 
 -- 4 Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 
-
-
-select first_name, last_name, datediff(now(), hire_date) as days_hired
+select first_name, last_name, count(last_name)
 from employees
-where hire_date between '1990-01-01' and '1999-12-31'
-and birth_date like '%-12-25'
-limit 5
+where last_name like 'e%e'
+group by last_name, first_name
 
--- 5 Find the smallest and largest salary from the salaries table.
+-- 5 Find the unique last names with a 'q' but not 'qu'. Your results should be:
 
-select *
-from salaries
-limit 5
+select last_name, count(last_name)
+from employees
+where last_name like '%q%' and last_name not like '%qu%'
+group by last_name
 
-describe salaries
+-- 6 Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
 
-emp_no	int(11)	NO	PRI	NULL	
-salary	int(11)	NO		NULL	
-from_date	date	NO	PRI	NULL	
-to_date	date	NO		NULL	
+select first_name, last_name, count(*)
+from employees
+group by first_name, last_name
+order by count(*) desc
+limit 20
 
-select min(salary), max(salary)
-from salaries
+-- 7 Update your query for 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
+ 
+select count(gender), gender
+from employees
+where first_name in ('Irena', 'Vidya','Maya')
+group by gender
 
--- 6 Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
 
-+------------+------------+-----------+------------+
-| username   | first_name | last_name | birth_date |
-+------------+------------+-----------+------------+
-| gface_0953 | Georgi     | Facello   | 1953-09-02 |
-| bsimm_0664 | Bezalel    | Simmel    | 1964-06-02 |
-| pbamf_1259 | Parto      | Bamford   | 1959-12-03 |
+-- THESE ARE JUST EXAMPLES FM READING
+-- ++++++++++++++++++++++++++++
 
-  -- NOTE - SUBSTR CAN ALSO USE NEG "-"
-select concat (lower(substr(first_name, 1, 1)), lower(substr(last_name, 1, 4)), "_", substr(birth_date, -5, 2), substr(birth_date, 3, 2)) as username, first_name, last_name, birth_date
-from `employees`
-limit 5
+SELECT first_name, COUNT(first_name)
+FROM employees
+WHERE first_name NOT LIKE '%a%'
+GROUP BY first_name
+limit 20
 
+SELECT hire_date, COUNT(*)
+FROM employees
+GROUP BY hire_date
+ORDER BY COUNT(*) DESC
+LIMIT 10;
+
+select first_name, last_name, count(*)
+from employees
+group by first_name, last_name
+order by count(*) desc
+limit 20
