52d299165d9c5f9ba8da5aa9f6b143618d26310c
Adam H <raymond.a.herman@gmail.com>
2019-09-08 21:35:24 -0500

    added new stuff

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -1,27 +1,49 @@
 -- 3.7_group_by_exercises.sql
+;
 
-
-use employees
+use employees;
 
 describe titles
 
 emp_no	int(11)	NO	PRI	NULL	
 title	varchar(50)	NO	PRI	NULL	
 from_date	date	NO	PRI	NULL	
-to_date	date	YES		NULL		
+to_date	date	YES		NULL;		
 
 -- 2 In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
 
 select distinct title
-from titles
+from titles;
 
+     -- output 2
+     Senior Engineer
+Staff
+Engineer
+Senior Staff
+Assistant Engineer
+Technique Leader
+Manager;
+
+-- vs no distinct
+;
+select title
+from titles;
 
 -- 3 Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
     
 select last_name, count(last_name)
 from employees
 where last_name like 'e%e'
-group by last_name
+group by last_name;
+
+	-- trying without the groupby
+	--- pancake doesn't like the comment on same line as a command
+	-- so [ select last_name -- count(last_name) ] does not work
+	-- must delete "count(last_name)" to run or put on own line
+;
+select last_name
+from employees
+where last_name like 'e%e';
 
 
 -- 4 Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
@@ -29,14 +51,14 @@ group by last_name
 select first_name, last_name, count(last_name)
 from employees
 where last_name like 'e%e'
-group by last_name, first_name
+group by last_name, first_name;
 
 -- 5 Find the unique last names with a 'q' but not 'qu'. Your results should be:
 
 select last_name, count(last_name)
 from employees
 where last_name like '%q%' and last_name not like '%qu%'
-group by last_name
+group by last_name;
 
 -- 6 Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
 
@@ -44,16 +66,24 @@ select first_name, last_name, count(*)
 from employees
 group by first_name, last_name
 order by count(*) desc
-limit 20
+limit 20;
 
 -- 7 Update your query for 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
  
 select count(gender), gender
 from employees
 where first_name in ('Irena', 'Vidya','Maya')
-group by gender
+group by gender;
 
 
+	-- just running a gender search 
+	select count(gender)
+    from employees
+    where first_name in ('Irena', 'Vidya','Maya');
+    
+	
+	
+	
 -- THIS ONE IS GIVING ME PROBLEMS
 -- 8 Recall the query the generated usernames for the employees from the last lesson. Are there any duplicate usernames?
 
@@ -63,13 +93,47 @@ select concat (lower(substr(first_name, 1, 1)), lower(substr(last_name, 1, 4)), 
 from `employees`
 group by username
 order by count(*)
-limit 5
+limit 5;
 
-select concat (lower(substr(first_name, 1, 1)), lower(substr(last_name, 1, 4)), "_", substr(birth_date, -5, 2), substr(birth_date, 3, 2)) as username, first_name, last_name, birth_date, count(*)
+select concat (lower(substr(first_name, 1, 1)), lower(substr(last_name, 1, 4)), "_", substr(birth_date, -5, 2), substr(birth_date, 3, 2)) as username, first_name, last_name, birth_date, count(username)
 from `employees`
 -- group by username
-order by count(*)
-limit 5
+group by username;
+limit 5;
+
+-- trying to copy notes form - 
+SELECT LOWER(CONCAT 
+              (SUBSTR(first_name,1,1),
+              SUBSTR(last_name,1,4),
+              '_',
+              SUBSTR(birth_date,6,2),
+              SUBSTR(birth_date,3,2)
+              )
+            ) AS username,
+            COUNT(*)
+ FROM employees
+ GROUP BY username
+ ORDER BY COUNT(*) DESC;
+
+
+-- 8 Bonus: how many duplicate usernames are there?
+-- Bonus: how many duplicate usernames are there?
+
+SELECT LOWER(CONCAT 
+              (SUBSTR(first_name,1,1),
+              SUBSTR(last_name,1,4),
+              '_',
+              SUBSTR(birth_date,6,2),
+              SUBSTR(birth_date,3,2)
+              )
+            ) AS username,
+            COUNT(*)
+ FROM employees
+ GROUP BY username
+ ORDER BY COUNT(*) DESC;
+
+
+
 
 
 -- THESE ARE JUST EXAMPLES FM READING
--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -1,5 +1,90 @@
 -- 3.9_temporary_tables.sql 
 
+
+-- 1 Using the example from the lesson, re-create the employees_with_departments table.
+;
+
+--  1 
+CREATE TEMPORARY TABLE employees_with_departments AS
+SELECT emp_no, first_name, last_name, dept_no, dept_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no);
+
+-- not with LIMIT 100;
+
+-- 	1a Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+;
+describe employees_with_departments;
+    -- output
+emp_no	int(11)	NO		NULL	
+first_name	varchar(14)	NO		NULL	
+last_name	varchar(16)	NO		NULL	
+dept_no	char(4)	NO		NULL	
+dept_name	varchar(40)	NO		NULL	;    
+
+
+ALTER TABLE employees_with_departments ADD full_name VARCHAR(30);
+-- actually USE 31 to absorb the SPACE between the CONCATS
+;
+
+ALTER TABLE employees_with_departments ADD full_name VARCHAR(31);
+
+-- droping and re-adding column - I bothced the VARCHAR legnth
+ALTER TABLE employees_with_departments DROP COLUMN full_name;
+
+--  b Update the table so that full name column contains the correct data
+;
+UPDATE employees_with_departments
+SET full_name = CONCAT(first_name, " ", last_name);
+
+		-- test
+		select * from employees_with_departments;
+		
+		
+-- USED THIS BECAUSE I BOTCHED AND REDID - Drop table employees_with_departments;
+--  c Remove the first_name and last_name columns from the table.
+
+ALTER TABLE employees_with_departments DROP COLUMN first_name;
+ALTER TABLE employees_with_departments DROP COLUMN last_name;
+
+
+--  d What is another way you could have ended up with this same table?
+;
+CREATE TEMPORARY TABLE employees_with_departments AS
+SELECT emp_no, dept_no, dept_name, concat (first_name, " ", last_name,) as full_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no);
+
+
+-- 2
+CREATE TEMPORARY TABLE payments AS
+select * from sakila.payment;
+
+select * from payments;
+
+alter table payments add cent_amount int unsigned;
+
+update payments
+set cent_amount = payments.amount * 100;
+
+select * from payments;
+
+
+
+
+
+
+
+
+
+
+
+
+
+--  ||||||||||||||||||||||||||||||||||||||||||||||||
+--  ||||||||||||||||||||||||||||||||||||||||||||||||
 select first_name, last_name, salary
 from employees
 join salaries on salaries.emp_no = employees.emp_no;
