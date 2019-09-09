7d65ef2db220868c98b139c2f0f33db4cd368909
Adam H <raymond.a.herman@gmail.com>
2019-09-09 12:06:54 -0500

    cant get Q1 but moved on Q3

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -1,12 +1,176 @@
 -- 3.11_more_exercises.sql t
 
-use Employees Database
+use Employees; Database
+;
+show databases;
+
+use employees;
 
 -- 1 How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary?
 
+describe departments;
+dept_no	char(4)	NO	PRI	NULL	
+dept_name	varchar(40)	NO	UNI	NULL;	
+
+describe dept_manager;
+emp_no	int(11)	NO	PRI	NULL	
+dept_no	char(4)	NO	PRI	NULL	
+from_date	date	NO		NULL	
+to_date	date	NO		NULL	;
+
+describe employees;
+emp_no	int(11)	NO	PRI	NULL	
+birth_date	date	NO		NULL	
+first_name	varchar(14)	NO		NULL	
+last_name	varchar(16)	NO		NULL	
+gender	enum('M','F')	NO		NULL	
+hire_date	date	NO		NULL	;
+
+describe salaries;
+emp_no	int(11)	NO	PRI	NULL	
+salary	int(11)	NO		NULL	
+from_date	date	NO	PRI	NULL	
+to_date	date	NO		NULL	;
+
+select emp_no, to_date
+from dept_manager;
+-- 9999-01-01
+
+--- relative to avg salary 
+;
+select avg(salary) as avg_sal
+from salaries;
+
+select *
+from dept_emp
+limit 5;
+
+10001	d005	1986-06-26	9999-01-01
+10002	d007	1996-08-03	9999-01-01
+10003	d004	1995-12-03	9999-01-01
+10004	d004	1986-12-01	9999-01-01
+10005	d003	1989-09-12	9999-01-01;
+
+select *
+from dept_manager
+limit 5;
+
+     -- fm 3.8. FINDING AVG DEPT SALARY
+     select d.dept_name, avg(s.salary) as average_salary
+from departments as d
+join dept_emp as de
+    on de.dept_no = d.dept_no
+join employees as e
+   on e.emp_no = de.emp_no
+join salaries as s
+   on s.emp_no = e.emp_no
+Where de.to_date = '9999-01-01' and s.to_date = '9999-01-01'
+group by d.dept_name
+order by average_salary DESC;
+
+      -- FM 3.8 - 2. GETTING DEPT & mgr
+      SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name 
+FROM employees AS e
+JOIN dept_manager AS dmgr
+  ON dmgr.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dmgr.dept_no
+WHERE dmgr.to_date = '9999-01-01'
+order by d.dept_name ASC;
 
 
-use World Database
+
+create temporary table dept_sal_avg AS;
+
+
+select d.dept_name, concat(e.first_name, ' ', e.last_name) as full_name, (s.salary - average_salary) as dif_salary
+
+;
+SELECT d.dept_name as 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS full_name, s.salary, (s.salary - average_salary) as dif_salary
+FROM employees AS e
+JOIN dept_manager AS dmgr
+  ON dmgr.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dmgr.dept_no
+Join salaries As s
+on s.emp_no  = e.emp_no
+WHERE dmgr.to_date = '9999-01-01' and s.to_date = '9999-01-01'
+    select departments.dept_name, avg(salalries.salary) as average_salary
+    from employees.departments as d
+        join employees.dept_emp as de
+          on de.dept_no = d.dept_no
+        join employees.employees as e
+          on e.emp_no = de.emp_no
+        join employees.salaries as s
+          on s.emp_no = e.emp_no
+     Where de.to_date = '9999-01-01' and s.to_date = '9999-01-01'
+order by d.dept_name;
+
+
+
+
+
+
+select d.dept_name, avg(s.salary) as average_salary
+from employees.departments as d
+join employees.dept_emp as de
+    on de.dept_no = d.dept_no
+join employees.employees as e
+   on e.emp_no = de.emp_no
+join employees.salaries as s
+   on s.emp_no = e.emp_no
+Where de.to_date = '9999-01-01' and s.to_date = '9999-01-01'
+group by d.dept_name
+order by average_salary DESC;
+
+-- |||||||||||||||||||||| use World Database
+
+;
+use world;
+
+describe city;
+ID	int(11)	NO	PRI	NULL	auto_increment
+Name	char(35)	NO			
+CountryCode	char(3)	NO	MUL		
+District	char(20)	NO			
+Population	int(11)	NO		0	;
+
+
+describe country;
+Code	char(3)	NO	PRI		
+Name	char(52)	NO			
+Continent	enum('Asia','Europe','North America','Africa','Oceania','Antarctica','South America')	NO		Asia	
+Region	char(26)	NO			
+SurfaceArea	float(10,2)	NO		0.00	
+IndepYear	smallint(6)	YES		NULL	
+Population	int(11)	NO		0	
+LifeExpectancy	float(3,1)	YES		NULL	
+GNP	float(10,2)	YES		NULL	
+GNPOld	float(10,2)	YES		NULL	
+LocalName	char(45)	NO			
+GovernmentForm	char(45)	NO			
+HeadOfState	char(60)	YES		NULL	
+Capital	int(11)	YES		NULL	
+Code2	char(2)	NO			;
+
+
+describe countrylanguage;
+CountryCode	char(3)	NO	PRI		
+Language	char(30)	NO	PRI		
+IsOfficial	enum('T','F')	NO		F	
+Percentage	float(4,1)	NO		0.0	;
+
+select cl.language, cl.percentage
+from city as c
+   join countrylanguage as cl
+       on cl.countrycode = c.countrycode
+where c.name = 'Santa Monica'
+order by cl.percentage ASC;
+
+;       
+select *
+from city
+limit 5;
 
 -- 1 What languages are spoken in Santa Monica?
 --  ex 1 +------------+------------+
@@ -18,8 +182,23 @@ use World Database
 | Korean     |        0.3 |
 | Polish     |        0.3 |;
 
+select cl.language, cl.percentage
+from city as c
+   join countrylanguage as cl
+       on cl.countrycode = c.countrycode
+where c.name = 'Santa Monica'
+order by cl.percentage ASC;
+
 -- 2 How many different countries are in each region?
 
+select *
+from country
+limit 2;
+
+select region, count(*) as num_countries
+from country
+group by region
+order by num_countries;
 
 +---------------------------+---------------+
 | Region                    | num_countries |
